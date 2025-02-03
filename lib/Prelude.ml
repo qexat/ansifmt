5934682107e170400ba987ba814949f051e04a51
Qexat <43090614+qexat@users.noreply.github.com>
2025-02-03 08:30:20 +0100

    Remove `parenthesize_if` completely Maybe I should try building before pushing 💀

--- lib/Prelude.ml
+++ lib/Prelude.ml
@@ -4,5 +4,4 @@ type stylizer = Formatting.Stylizer.t
 
 let make_styling = Styling.create
 let format = Formatting.Util.format
-let parenthesize_if = Formatting.Util.parenthesize_if
 let print_formatted = IO.print_formatted
