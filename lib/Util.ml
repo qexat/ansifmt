72a939e823a1a4c24d1360f3c24221384f6c1e15
Qexat <43090614+qexat@users.noreply.github.com>
2025-02-20 16:39:41 +0100

    util(internal): add `List.intersperse`

--- lib/Util.ml
+++ lib/Util.ml
@@ -117,6 +117,22 @@ module Triplet = struct
   ;;
 end
 
+module List = struct
+  include List
+
+  (** [intersperse item list] inserts [item] between every
+      element of [list]. This function is tail-recursive. *)
+  let intersperse : 'a -> 'a t -> 'a t =
+    let rec tailrec item list acc =
+      match list with
+      | [] -> rev acc
+      | last :: [] -> tailrec item [] (last :: acc)
+      | first :: rest -> tailrec item rest (item :: first :: acc)
+    in
+    fun item list -> tailrec item list []
+  ;;
+end
+
 module Option = struct
   include Option
 
