b72c744dd833c0240458606736cf85e9cac3272f
Qexat <43090614+qexat@users.noreply.github.com>
2025-02-21 09:47:52 +0100

    internal: fix `List.intersperse`
    Co-authored-by: Dmitrii Kovanikov <kovanikov@gmail.com>

--- lib/Internal/List.ml
+++ lib/Internal/List.ml
@@ -5,12 +5,10 @@ let singleton : 'a -> 'a t = fun element -> [ element ]
 
 (** [intersperse item list] inserts [item] between every
       element of [list]. This function is tail-recursive. *)
-let intersperse : 'a -> 'a t -> 'a t =
-  let[@tail_mod_cons] rec tailrec item list =
-    match list with
-    | [] -> []
-    | [last] -> [last]
-    | first :: rest -> first :: item :: tailrec item rest
-  in
-  fun item list -> tailrec item list []
+let[@tail_mod_cons] rec intersperse : 'a -> 'a t -> 'a t =
+  fun item list ->
+  match list with
+  | [] -> []
+  | [ last ] -> [ last ]
+  | first :: rest -> first :: item :: intersperse item rest
 ;;
