44a4b4df04f601b2f5885680fc5076dad8fc8e03
lexa <contact@qexat.com>
2025-02-21 09:42:47 +0100

    internal: `List.intersperse`: use `tail_mod_cons`
    Co-authored-by: Dmitrii Kovanikov <kovanikov@gmail.com>

--- lib/Internal/List.ml
+++ lib/Internal/List.ml
@@ -6,11 +6,11 @@ let singleton : 'a -> 'a t = fun element -> [ element ]
 (** [intersperse item list] inserts [item] between every
       element of [list]. This function is tail-recursive. *)
 let intersperse : 'a -> 'a t -> 'a t =
-  let rec tailrec item list acc =
+  let[@tail_mod_cons] rec tailrec item list =
     match list with
-    | [] -> rev acc
-    | last :: [] -> tailrec item [] (last :: acc)
-    | first :: rest -> tailrec item rest (item :: first :: acc)
+    | [] -> []
+    | [last] -> [last]
+    | first :: rest -> first :: item :: tailrec item rest
   in
   fun item list -> tailrec item list []
 ;;
